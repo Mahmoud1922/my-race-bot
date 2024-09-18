dba7fb49e3d8881f3d463ce7ca1d52200d4ddcee
mahmoudt <mahmoud.talebi@nobleo.nl>
2024-09-18 19:54:15 +0200

    It is fast!

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ from pygame import Vector2
 from ...bot import Bot
 from ...linear_math import Transform
 
-max_velocity = 200
+max_velocity = 300
+look_ahead_distance = 100.0
 
 class ComplicatedBot(Bot):
 
@@ -20,7 +21,7 @@ class ComplicatedBot(Bot):
 
     @property
     def contributor(self):
-        return "Nobleo"
+        return "Mahmoud"
 
     def compute_commands(self, next_waypoint: int, position: Transform, velocity: Vector2) -> Tuple:
         target = self.track.lines[next_waypoint]
@@ -30,7 +31,6 @@ class ComplicatedBot(Bot):
             self.initial_distance = (position.p - target).length()
             if self.initial_distance == 0:
                 self.initial_distance = 1
-            print("Calcuated initial distance: ", self.initial_distance)
         
         # calculate the target in the frame of the robot
         target = position.inverse() * target
@@ -38,8 +38,11 @@ class ComplicatedBot(Bot):
         angle = target.as_polar()[1]
 
         # calculate the throttle
-        current_distance = target.length()
-        velocity_factor = (1 - abs(angle) / 180) * (current_distance / self.initial_distance)
+        distance_factor = (target.length() / self.initial_distance)
+        if(target.length() > look_ahead_distance):
+            distance_factor = 1.0
+        
+        velocity_factor = (1 - abs(angle) / 180) * distance_factor
         target_velocity = max_velocity * velocity_factor
         if velocity.length() < target_velocity:
             throttle = 1
